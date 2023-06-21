af0aebbd3c7475016fbb674addbe77545fcad455
unknown <bhangusimran530@gmail.com>
2023-06-21 14:25:30 -0400

    first commit

---
+++ Patient.cs
@@ -0,0 +1,91 @@
+﻿using System;
+
+namespace Csharp_Assignment_1
+{
+    public class Patient
+    {
+        // Properties
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public double Weight { get; set; } // in KG
+        public double Height { get; set; } // in Centimeters
+
+        // Custom constructor
+        public Patient(string firstName, string lastName, double weight, double height)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Weight = weight;
+            Height = height;
+        }
+
+        // Method to calculate BMI
+        public double CalculateBMI()
+        {
+            double heightInMeters = Height / 100; // Convert height from centimeters to meters
+            return Weight / (heightInMeters * heightInMeters);
+        }
+
+        // Method to calculate blood pressure
+        public static string CalculateBloodPressure(int systolic, int diastolic)
+        {
+            if (systolic < 0 || diastolic < 0)
+            {
+                return "Invalid blood pressure values";
+            }
+
+            if (systolic < 120 && diastolic < 80)
+            {
+                return "NORMAL";
+            }
+            else if (systolic < 130 && diastolic < 80)
+            {
+                return "ELEVATED";
+            }
+            else if (systolic < 140 || diastolic < 90)
+            {
+                return "HIGH BLOOD PRESSURE (HYPERTENSION) STAGE 1";
+            }
+            else if (systolic < 180 || diastolic < 120)
+            {
+                return "HIGH BLOOD PRESSURE (HYPERTENSION) STAGE 2";
+            }
+            else if (systolic >= 180 || diastolic >= 120)
+            {
+                return "HYPERTENSIVE CRISIS (consult your doctor immediately)";
+            }
+            else
+            {
+                return "Blood pressure values not in range";
+            }
+        }
+
+        // Method to output patient information
+        public void OutputPatientInformation()
+        {
+            double bmi = CalculateBMI();
+            string bmiStatus = "";
+
+            if (bmi >= 25.0)
+            {
+                bmiStatus = "Overweight";
+            }
+            else if (bmi >= 18.5 && bmi < 25.0)
+            {
+                bmiStatus = "Normal (In the healthy range)";
+            }
+            else
+            {
+                bmiStatus = "Underweight";
+            }
+
+            Console.WriteLine("Patient Information:");
+            Console.WriteLine("Full Name: {0} {1}", FirstName, LastName);
+            Console.WriteLine("Weight: {0} KG", Weight);
+            Console.WriteLine("Height: {0} CM", Height);
+            Console.WriteLine("Blood Pressure: {0}", CalculateBloodPressure(120,80));
+            Console.WriteLine("BMI: {0}", bmi);
+            Console.WriteLine("BMI Status: {0}", bmiStatus);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿
+//SIMRANJIT  KAUR
+//200536453
+//ASSIGNMENT 1
+    using Csharp_Assignment_1;
+using System;
+
+namespace Csharp_Assignment_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Please provide your age:");
+            int age = int.Parse(Console.ReadLine());
+
+            // Prompt user to enter patient information
+            Console.WriteLine("Please enter patient's information:");
+            Console.Write("First Name: ");
+            string firstName = Console.ReadLine();
+            Console.Write("Last Name: ");
+            string lastName = Console.ReadLine();
+            Console.Write("Weight (KG): ");
+            double weight = double.Parse(Console.ReadLine());
+            Console.Write("Height (CM): ");
+            double height = double.Parse(Console.ReadLine());
+
+            // Create a new patient object with the entered information
+            Patient patient = new Patient(firstName, lastName, weight, height);
+
+            // Output the patient's information and calculated values
+            patient.OutputPatientInformation();
+
+            Console.ReadLine();
+        }
+    }
+}
